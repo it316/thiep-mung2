50904b553d1dbb026ff335165bc49664c75a9995
it316 <143130836+it316@users.noreply.github.com>
2023-12-10 04:23:57 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Ms. Màu';
+const giftUrl = 'https://it316.github.io/thiep-mung/';
+const eventName = 'Happy Birthday ';
+const titleCard = 'Tặng Cô Màu ';
+const contentCard = 'Chúc mừng sinh nhật cô Màu con mong cô luôn khỏe mạnh cô đã giúp con rất nhiều trong suốt những năm tháng qua Con thật sự cảm ơn cô vì đã xuất hiện trong cuộc sống của con.Con mong cô luôn mạnh khỏe Con Cảm Ơn Cô ';
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
